ef4850bf7b1aadb51f5bd67009185bf0ca5d9898
the3plet <soorajp2002@gmail.com>
2023-11-29 23:30:08 +0530

    button alignment

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -8,15 +8,15 @@ const Controller = () => {
       </Box>
       <Box sx={{ display: "flex", justifyContent: "space-between",pt:'20px',px:'30px' }}>
         <Button variant="contained">Left</Button>
-        <Button variant="contained" sx={{ml:'15px'}}>Stop</Button>
+        <Button variant="contained" color="error" sx={{ml:'15px'}}>Stop</Button>
         <Button variant="contained">Right</Button>
       </Box>
       <Box sx={{ display: "flex", justifyContent: "center", pt:'20px'}}>
         <Button variant="contained">Backward</Button>
       </Box>
       <Box sx={{ display: "flex", justifyContent: "center", pt:'90px',gap:'20px'}}>
-        <Button variant="contained">Conveyor Start</Button>
-        <Button variant="contained">Conveyor Stop</Button>
+        <Button variant="outlined" color="success">Conveyor Start</Button>
+        <Button variant="contained" color="error">Conveyor Stop</Button>
       </Box>
     </Box>
   );
